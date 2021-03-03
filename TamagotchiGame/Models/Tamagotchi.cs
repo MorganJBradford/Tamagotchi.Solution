8643a94b68b1758e5e519b1903c04d5594375ba8
Morgan Bradford <morganjbradford95@gmail.com>
2021-03-03 13:12:23 -0600

    ReturnsSleepiness; good fail
    Co-authored-by: Mike Pingel <mdpingel@gmail.com>

--- TamagotchiGame/Models/Tamagotchi.cs
+++ TamagotchiGame/Models/Tamagotchi.cs
@@ -5,12 +5,14 @@ namespace TamagotchiGame.Models
     public string Name { get; set; }  
     public int Hunger { get; }
     public int Attention { get; }
+    public int Sleepiness { get; }
 
     public Tamagotchi (string name)
     {
       Name = name;
       Hunger = 100;
       Attention = 100;
+      Sleepiness = 100;
     }
     
   }
